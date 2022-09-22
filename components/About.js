32c53a76b573cb2975b3999e2da7903e5e42d695
SamChav <chavez.samuel1@gmail.com>
2022-09-21 20:29:56 -0700

    had to remove photo not rendering on deployed version

--- components/About.js
+++ components/About.js
@@ -9,8 +9,7 @@ const About = (props) => {
       <p className={styles.paragraph}>This site was designed with Next.js, React, JavaScript, Hero Icons, and React Color, with more to come. I can build full stack applications utilizing Express, PostgreSQL, MySQL, RESTful API development, React, Tailwind, Threejs, HTML, ES6, and CSS.  </p>
       <p className={styles.paragraph}>I currently have hundreds of hours of coding experience and I love programming! I'm eager to learn, moldable, and I am often told by colleagues that I am very relaxed. I hope to be a great fit in my upcoming work environment.
       </p>
-      <p className={styles.paragraph}>I have a wonderful family of five who supports me in my coding endeavors. I'm grateful for all that I have!</p>   
-      <div className={styles.imageDiv}><Image src="/christmas.png" layout="fill" objectPosition="center" className={styles.imageDiv} /></div>
+      <p className={styles.paragraph}>I have a wonderful family of five who supports me in my coding endeavors. I'm grateful for all that I have!</p>  
       </div>
     </div>
   );

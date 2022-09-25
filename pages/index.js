336b3e355178e54439c530962dc1028517f061a5
SamChav <chavez.samuel1@gmail.com>
2022-09-24 19:13:07 -0700

    small about me updates

--- pages/index.js
+++ pages/index.js
@@ -13,19 +13,18 @@ const App = () => {
         <div className={styles.samplesContainer}>
           <div className={styles.aboutText}>
             <p className={styles.paragraph}>
-              Hi, and welcome to my portfolio! Currently, this is a work in
-              progress. I do plan to have this complete in the next few weeks,
-              but I'll always be improving.
+              Hi, and welcome to my portfolio! This site is under permanent construction as I will always be improving it.
             </p>
             <p className={styles.paragraph}>
               This site was designed with Next.js, React, JavaScript, Hero
               Icons, and React Color, with more to come. I can build full stack
               applications utilizing Express, PostgreSQL, MySQL, RESTful API
-              development, React, Tailwind, Threejs, HTML, ES6, and CSS.{" "}
+              development, React, Tailwind, Threejs, HTML, ES6, and CSS. I Have built and rebuilt small applications you can find
+              on this site from scratch to demonstrate some of my proficiency as well as links to the code on my github.
             </p>
             <p className={styles.paragraph}>
-              I currently have hundreds of hours of coding experience and I love
-              programming! I'm eager to learn, moldable, and I am often told by
+              I currently have over 1,000 of hours of coding experience and I love
+              programming! I am open to remote, relocation, and in-person work. I'm eager to learn, moldable, and I am often told by
               colleagues that I am very relaxed. I hope to be a great fit in my
               upcoming work environment.
             </p>
